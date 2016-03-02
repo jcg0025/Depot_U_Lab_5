306a7eef87eb2ed12b694652c51562f5cac346c1
Jacob Gossett <jgossett121@gmail.com>
2016-03-02 00:59:10 -0600

    whatever

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('#keyboard-upper-container').hide();
-
+    $('#glyph-ok').hide();
+    $('#glyph-remove').hide();
 
 $(document).keydown(function(key){
     if (key.which == 16) {
@@ -48,26 +49,73 @@ $('#words').append(sentences[0]);
 $('#words-typed').append('<input></input>');
 
 
+
 var letterArray =[];
 $(document).keypress(function(key) {
-    var glyphOK = $('<span></span>').addClass('glyphicon glyphicon-ok');
-    var glyphRemove =$('<span></span>').addClass('glyphicon glyphicon-remove');
     var letterTyped = String.fromCharCode(key.which);
     letterArray.push(letterTyped);
-    console.log(letterArray);
-    console.log(letterArray.length); 
     var x = letterArray.length -1;
     var y = letterArray.length - 49;
-    if (letterArray[x] === sentences[0].charAt(x)) {
-        console.log('match');
-        $('.glyph-container').append(glyphOK);
-    } else {
-        $('.glyph-container').append(glyphRemove);
+    var z = letterArray.length - 97;
+    var a = letterArray.length - 145;
+    console.log(letterArray.length);
+   
+    
+    // var moveBlock = function () {
+    //     var pixels = String(x * 17.5);
+    //     $('#block').css('margin-left', pixels.concat('px'));
+    // };
+    // var blockBack = function() {
+    //     var pixels = String(x/17.5);
+    //     $('#block').css('margin-left', pixels.concat('px'));
+    // }
+    
+    switch (x) {
+        case 46:
+            $('#words2').append(sentences[1]);
+            $('#words').hide();
+            break;
+        case 97:
+            $('#words3').append(sentences[2]);
+            $('#words2').hide();
+            break;
+        case 145:
+            $('#words4').append(sentences[3]);
+            $('#words3').hide();
+            break;
+        case 193:
+            $('#words5').append(sentences[4]);
+            $('#words4').hide();
+            break;
     }
-    if (x == 48) {
-        $('#words').hide(sentences[0]);
-        $('#words2').append(sentences[1]);
+    
+    var typedLetter = letterArray[x];
+    switch (typedLetter) {
+        case sentences[0].charAt(x):
+            $('#glyph-ok').show();
+            $('#glyph-remove').hide();
+            break;
+        case sentences[1].charAt(y):
+            $('#glyph-ok').show();
+            $('#glyph-remove').hide();
+            break;
+        case sentences[1].charAt(z):
+            $('#glyph-ok').show();
+            $('#glyph-remove').hide();
+            break;
+        case sentences[1].charAt(a):
+            $('#glyph-ok').show();
+            $('#glyph-remove').hide();
+            break;
+        default:
+            $('#glyph-remove').show(); 
+            $('#glyph-ok').hide();  
+             
+ 
     }
+
+    
+        
 
     
         
